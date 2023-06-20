868dc8eedaea66e8c3b6892a38125400dee01758
fivge <luanxingtong@gmail.com>
2023-06-21 00:12:10 +0800

    feat: add proxy, dotenv

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,26 @@ import cors from "@fastify/cors";
 
 import Routes from "./routes/product-routes";
 
-const app: FastifyInstance = Fastify({ logger: false });
+const app: FastifyInstance = Fastify({ logger: process.env.PROD === "true" });
 
 app.register(cors, {
   origin: true,
   methods: "PUT, POST, GET, DELETE, OPTIONS",
-  allowedHeaders: "Content-Type, Content-Length, Authorization, Accept, X-Requested-With",
+  allowedHeaders: "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, x-session-id",
   optionsSuccessStatus: 204,
   strictPreflight: false,
+});
+
+app.register(require("@fastify/http-proxy"), {
+  upstream: process.env.SHIORI_URL,
+  prefix: "/shiori",
+  replyOptions: {
+    // rewriteRequestHeaders: (originalReq, headers) => ({
+    //   ...headers,
+    //   "access-control-allow-headers": `Content-Type, Content-Length, Authorization, Accept, X-Requested-With, x-session-id`,
+    // }),
+  },
+  http2: false,
 });
 
 app.register(Routes);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import "dotenv/config";
 import app from "./app";
 
-const PORT = 3000;
+const PORT = +process.env.PORT!;
 
 const start = async () => {
   try {
-    await app.listen({ port: PORT, host: "0.0.0.0" });
+    app.listen({ port: PORT, host: "0.0.0.0" });
     console.log("server start on", PORT);
   } catch (err) {
     app.log.error(err);
